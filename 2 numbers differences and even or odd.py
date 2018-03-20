e5dc9fdcbce049c089516e96a13ff6e639fd099e
Ardhra123 <36723000+Ardhra123@users.noreply.github.com>
2018-03-20 16:26:52 +0530

    Create 2 numbers differences and even or odd.py

---
+++ 2 numbers differences and even or odd.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if abs(a-b) == abs(b-c) :
+    print("True")
+else :
+    print("False")
+    num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
