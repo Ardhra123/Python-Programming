74e0df68e7f8d91c5942ae03e47fc8c2aca79fb2
Ardhra123 <36723000+Ardhra123@users.noreply.github.com>
2018-03-21 16:02:24 +0530

    Rename isogram.py to isogram 1.py

---
+++ isogram 1.py
@@ -0,0 +1,13 @@
+def is_isogram(string):
+    if isinstance(string, str) and len(string) != 0:
+        string = string.lower()
+        if len(string) == len(set(string)):
+            result = True
+        else:
+            result = False
+    elif not string:
+      result = False
+    else:
+        raise TypeError('Argument should be a string')
+
+    return string, result
