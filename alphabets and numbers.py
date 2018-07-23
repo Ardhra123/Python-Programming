208067dcf9a688c44048fb9bbc12469880abc092
Ardhra123 <36723000+Ardhra123@users.noreply.github.com>
2018-07-23 11:39:07 +0530

    Create alphabets and numbers.py

---
+++ alphabets and numbers.py
@@ -0,0 +1,14 @@
+def count(x):
+    length = len(x)
+    digit = 0
+    letters = 0
+    other = 0
+    for i in x:
+        if x[i].isalpha():
+            letters += 1
+        elif x[i].isnumeric():
+            digit += 1
+
+        else:
+            other += 1
+    return number,word,other
