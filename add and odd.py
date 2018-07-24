e0ce8c91cb3cfdef1845628820e026f6c52c62f6
Ardhra123 <36723000+Ardhra123@users.noreply.github.com>
2018-07-24 11:40:26 +0530

    Create add and odd.py

---
+++ add and odd.py
@@ -0,0 +1,13 @@
+num1 = 5
+num2 = 6
+
+
+sum = float(num1) + float(num2)
+
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+num = int(input("Enter the number "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
