e662f1b0dd24e050f59338e81f8907fef2d1964b
Ardhra123 <36723000+Ardhra123@users.noreply.github.com>
2018-08-13 15:26:33 +0530

    Create decimals.py

---
+++ decimals.py
@@ -0,0 +1,16 @@
+a = 3.1415926
+b = -12.9999
+c = 314.987
+d = 11.09875
+e = 20.987656
+print("\nOriginal Number: ", a)
+print("Formatted Number with sign: "+"{:+.5f}".format(a));
+print("Original Number: ", b)
+print("Formatted Number with sign: "+"{:+.5f}".format(b));
+print("\nOriginal Number: ", c)
+print("Formatted Number with sign: "+"{:+.5f}".format(c));
+print("Original Number: ", d)
+print("Formatted Number with sign: "+"{:+.5f}".format(d));
+print("Original Number: ", e)
+print("Formatted Number with sign: "+"{:+.5f}".format(e));
+print()
